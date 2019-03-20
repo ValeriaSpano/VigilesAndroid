de6d48cca9ccbc41b3e28a80aafcc03ea2980ff6
ValeriaSpano <valeria.spano@ied.edu>
2019-03-20 13:57:52 +0100

    File java+xml
    Aggiunta di un file java e xml per ogni sezione (il passaggio da una all’altra funziona)

--- VigilesAndroid/app/src/main/java/com/ied/vigiles/SearchActivity.java
+++ VigilesAndroid/app/src/main/java/com/ied/vigiles/SearchActivity.java
@@ -4,26 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import com.ied.vigiles.SearchActivity;
-import com.ied.vigiles.ReportActivity;
-import com.ied.vigiles.HistoryActivity;
-import com.ied.vigiles.ProfileActivity;
+import com.ied.vigiles.MapsActivity;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class SearchActivity extends AppCompatActivity {
 
-    private GoogleMap mMap;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -34,52 +23,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    Intent g = new Intent(SearchActivity.this, MapsActivity.class);
+                    startActivity(g);
                     return true;
                 case R.id.navigation_search:
-                    Intent h = new Intent(MapsActivity.this, SearchActivity.class);
-                    startActivity(h);
+                    mTextMessage.setText(R.string.title_search);
                     return true;
                 case R.id.navigation_reports:
                     mTextMessage.setText(R.string.title_reports);
-                    Intent i = new Intent(MapsActivity.this, ReportActivity.class);
+                    Intent i = new Intent(SearchActivity.this, ReportActivity.class);
                     startActivity(i);
-                    return true;
                 case R.id.navigation_historical:
                     mTextMessage.setText(R.string.title_historical);
-                    Intent j = new Intent(MapsActivity.this, HistoryActivity.class);
+                    Intent j = new Intent(SearchActivity.this, HistoryActivity.class);
                     startActivity(j);
                     return true;
                 case R.id.navigation_profile:
                     mTextMessage.setText(R.string.title_profile);
-                    Intent k = new Intent(MapsActivity.this, ProfileActivity.class);
+                    Intent k = new Intent(SearchActivity.this, ProfileActivity.class);
                     startActivity(k);
                     return true;
             }
             return false;
         }
-
     };
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setContentView(R.layout.activity_search);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
